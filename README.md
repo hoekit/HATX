d4ec4963be1452b5fc2f4b22675d244cb1c7e724
Hoe Kit CHEW <hoekit@gmail.com>
2024-06-26 12:41:06 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+HATX - Blah blah blah
+
+# SYNOPSIS
+
+    use HATX;
+
+# DESCRIPTION
+
+HATX is
+
+# AUTHOR
+
+Hoe Kit CHEW <hoekit@gmail.com>
+
+# COPYRIGHT
+
+Copyright 2024- Hoe Kit CHEW
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/HATX.pm
@@ -0,0 +1,39 @@
+package HATX;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+HATX - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use HATX;
+
+=head1 DESCRIPTION
+
+HATX is
+
+=head1 AUTHOR
+
+Hoe Kit CHEW E<lt>hoekit@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2024- Hoe Kit CHEW
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
